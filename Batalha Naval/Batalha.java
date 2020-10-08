5d544b770e007b16cd3d47d5ea43fbbed2712987
João Pedro <62775155+JoaoPedro-M@users.noreply.github.com>
2020-10-08 19:18:16 -0300

    update

--- Batalha Naval/Batalha.java
+++ Batalha Naval/Batalha.java
@@ -1,10 +1,11 @@
-
-class Batalha {
+public class Batalha {
     public static void main(String[] args){
         Dog cachorro = new Dog();
         System.out.println(cachorro.getLatido());
         cachorro.setLatido(true);
         System.out.println(cachorro.getLatido());
+        Arnaldo ar = new Arnaldo();
+        ar.falar();
     }
 }
 
@@ -17,5 +18,16 @@ class Dog {
 
     public void setLatido(boolean late){
         latir = late;
+    }
+}
+
+
+
+
+class Arnaldo {
+    private int casa = 5;
+
+    public void falar(){
+        System.out.println(casa);
     }
 }
---
+++ Batalha Naval/mas/Carro.java
@@ -0,0 +1,16 @@
+public class Carro {
+    public static void main(String[ ] args){
+        System.out.println("Hello World!");
+        Voar v = new Voar();
+        v.Andar();
+    }
+}
+
+
+
+
+class Voar {
+    public void Andar() {
+        System.out.println("bibocas");
+    }
+}
