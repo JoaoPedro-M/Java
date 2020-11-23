cd036953201735ee6900691505094d16de1e1555
João Pedro <62775155+JoaoPedro-M@users.noreply.github.com>
2020-11-23 17:28:51 -0300

    Update BeatBox.java

--- Batalha Naval/guis/gui-feita/BeatBox.java
+++ Batalha Naval/guis/gui-feita/BeatBox.java
@@ -174,14 +174,16 @@ public class BeatBox {
     }
 
 
+    public class MySendListener implements ActionListener {
+        public void actionPerformed(ActionEvent a) {
 
-
-
-
-
-
-
-
+            boolean[] checkboxState = new boolean[256];
+            
+            for (int i = 0; i < 256; i++) {
+                JCheckBox check = (JCheckBox) checkboxList.get(i);
+            }
+        }
+    }
 
 
 
